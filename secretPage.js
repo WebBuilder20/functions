ed861d399be984eb400abb573ff3235338f045dc
WebBuilder20 <localhost.ads@gmail.com>
2020-07-09 12:43:56 +0900

    Update secretPage.js

--- secretPage.js
+++ secretPage.js
@@ -1,8 +1,11 @@
 // ts -> js : 터미널에 tsc secretPage.ts
 var ERROR_notProperSC = "ERROR : 비정상적인 유튜브 소스 코드. 복사부터 다시해서 붙여넣기.";
-var ERROR_LengWH = 'ERROR : 가로와 세로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
-var ERROR_LengW = 'ERRO : 가로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
-var ERROR_LengH = 'ERROR : 세로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
+var ERROR_LengWH_YT = 'ERROR : 가로와 세로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
+var ERROR_LengW_YT = 'ERRO : 가로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
+var ERROR_LengH_YT = 'ERROR : 세로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
+var ERROR_LengWH_IMG = 'ERROR : 가로와 세로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
+var ERROR_LengW_IMG = 'ERRO : 가로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
+var ERROR_LengH_IMG = 'ERROR : 세로 길이가 입력되지 않음. 1500 * 800으로 두고 싶으면, 앞에 체크박스 풀기.';
 var ERROR_INW_LengWH = 'ERROR : 원본 HTML 이미지 길이가 입력되지 않음. 이미 제대로 변환되고 있었다면 아래 체크박스 풀기.';
 var ERROR_INW_LengW = 'ERRO : 원본 HTML 이미지 가로 길이가 입력되지 않음. 이미 제대로 변환되고 있었다면 아래 체크박스 풀기.';
 var ERROR_INW_LengH = 'ERROR : 원본 HTML 이미지 세로 길이가 입력되지 않음. 이미 제대로 변환되고 있었다면 아래 체크박스 풀기.';
@@ -39,13 +42,13 @@ function sourceConvert() {
         if (widthV == "" || heightV == "") {
             // resize == true && ERROR : numbers not inserted
             if (widthV == "" && heightV == "") {
-                alert(ERROR_LengWH);
+                alert(ERROR_LengWH_YT);
             }
             else if (widthV == "") {
-                alert(ERROR_LengW);
+                alert(ERROR_LengW_YT);
             }
             else if (heightV == "") {
-                alert(ERROR_LengH);
+                alert(ERROR_LengH_YT);
             }
         }
         else {
@@ -148,13 +151,13 @@ function image_convert() {
     var width_wanted = document.getElementById("image_width").value;
     var height_wanted = document.getElementById("image_height").value;
     if (width_wanted == "" && height_wanted == "") {
-        alert(ERROR_LengWH);
+        alert(ERROR_LengWH_IMG);
     }
     else if (width_wanted == "") {
-        alert(ERROR_LengW);
+        alert(ERROR_LengW_IMG);
     }
     else if (height_wanted == "") {
-        alert(ERROR_LengH);
+        alert(ERROR_LengH_IMG);
     }
     else {
         if (document.getElementById("INW_checkBox").checked == true) {
