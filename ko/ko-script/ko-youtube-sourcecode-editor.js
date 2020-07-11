b2ec8cdcc935a73539a946069dfe527442b4db30
WebBuilder20 <localhost.ads@gmail.com>
2020-07-11 13:34:55 +0900

    Create ko-youtube-sourcecode-editor.js

---
+++ ko/ko-script/ko-youtube-sourcecode-editor.js
@@ -0,0 +1,109 @@
+// ts -> js : 터미널에 tsc youtube-sourcecode-editor.ts
+var ERROR_notProperSC = "ERROR : 유튜브 소스코드가 정상적이지 않습니다.";
+var ERROR_LengWH = 'ERROR : 가로와 세로 길이가 정상 입력되지 않았습니다.';
+var ERROR_LengW = 'ERROR : 가로 길이가 정상 입력되지 않았습니다.';
+var ERROR_LengH = 'ERROR : 세로 길이가 정상 입력되지 않았습니다.';
+function youtube_copy() {
+    var copyText = document.getElementById("youtube_after");
+    copyText.select();
+    document.execCommand("copy");
+    copyText.setSelectionRange(0, 0);
+}
+function youtube_numberLength() {
+    if (this.value.length > this.maxLength) {
+        this.value = this.value.slice(0, this.maxLength);
+    }
+}
+var changeSize = false;
+function youtube_openText() {
+    if (changeSize == false) {
+        changeSize = true;
+        document.getElementById("youtube_width").disabled = false;
+        document.getElementById("youtube_height").disabled = false;
+    }
+    else {
+        changeSize = false;
+        document.getElementById("youtube_width").disabled = true;
+        document.getElementById("youtube_height").disabled = true;
+    }
+}
+function sourceConvert() {
+    var loop = document.getElementById("youtube_loop").checked;
+    var autoplay = document.getElementById("youtube_autoplay").checked;
+    var sourceCode = document.getElementById("youtube_before").value;
+    var DoNotConvert = false;
+    var widthV = document.getElementById("youtube_width").value;
+    var heightV = document.getElementById("youtube_height").value;
+    if (changeSize == true) {
+        if (widthV == "" || heightV == "") {
+            // resize == true && ERROR : numbers not inserted
+            if (widthV == "" && heightV == "") {
+                alert(ERROR_LengWH);
+            }
+            else if (widthV == "") {
+                alert(ERROR_LengW);
+            }
+            else if (heightV == "") {
+                alert(ERROR_LengH);
+            }
+        }
+        else {
+            // resize = true && numbers inserted && other functions
+            if (sourceCode.includes('width="') && sourceCode.includes('height="')) {
+                var width = document.getElementById("youtube_width").value;
+                var height = document.getElementById("youtube_height").value;
+                var findWidth = sourceCode.split('width="');
+                if (findWidth[1].includes('"')) {
+                    var widthIndex = findWidth[1].indexOf('"') + 1;
+                    var afterWidth = findWidth[1].substr(widthIndex);
+                    sourceCode = findWidth[0] + ("width=\"" + width + "\"") + afterWidth;
+                }
+                else {
+                    DoNotConvert = true;
+                }
+                var findHeight = sourceCode.split('height="');
+                if (findHeight[1].includes('"')) {
+                    var heightIndex = findHeight[1].indexOf('"');
+                    var afterHeight = findHeight[1].substr(heightIndex + 1);
+                    sourceCode = findHeight[0] + ("height=\"" + height + "\"") + afterHeight;
+                }
+                else {
+                    DoNotConvert = true;
+                }
+            }
+            else {
+                DoNotConvert = true;
+            }
+            otherThanSize();
+        }
+    }
+    else {
+        otherThanSize();
+    }
+    function otherThanSize() {
+        if (sourceCode.includes("embed/") && sourceCode.includes('"')) {
+            var youtubeId_split1 = sourceCode.split("embed/");
+            var youtubeId_split2 = youtubeId_split1[1].split('"');
+            var youtubeId = youtubeId_split2[0];
+            var youtube_split3 = sourceCode.split(youtubeId);
+            if (autoplay && loop) {
+                sourceCode = youtube_split3[0] + youtubeId + "?version=3&autoplay=1&loop=1&playlist=" + youtubeId + youtube_split3[1];
+            }
+            else if (autoplay) {
+                sourceCode = youtube_split3[0] + youtubeId + "?version=3&autoplay=1&playlist=" + youtubeId + youtube_split3[1];
+            }
+            else if (loop) {
+                sourceCode = youtube_split3[0] + youtubeId + "?version=3&loop=1&playlist=" + youtubeId + youtube_split3[1];
+            }
+        }
+        else {
+            DoNotConvert = true;
+        }
+        if (!DoNotConvert) {
+            document.getElementById("youtube_after").value = sourceCode;
+        }
+        else {
+            alert(ERROR_notProperSC);
+        }
+    }
+}
